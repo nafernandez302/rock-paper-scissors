2db7f1b64db36cbcb0eab306493894dfdb625a58
ferndz <nafernandez@mi.unc.edu.ar>
2024-01-16 21:43:33 -0300

    comenzando DOM

--- script.js
+++ script.js
@@ -20,7 +20,11 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    setPlayerChoice(playerSelection);
+    setComputerChoice(computerSelection);
     incrementMatchNumber();
+    const output = document.querySelector("#output-event");
+    output.appendChild(gameToMessage(game));
     console.log(`Match number ${game.matchNumber}`);
     console.log(`Player's choice: ${playerSelection}`);
     console.log(`Computer's choice: ${computerSelection}`);
@@ -77,6 +81,25 @@ function resetGame(){
     game.matchNumber = 0;
 }
 
+function gameToMessage(game){
+    const messageContainer = document.createElement("div");
+    const matchPrint = document.createElement("p");
+    const playerElection = document.createElement("p");
+    const computerElection = document.createElement("p");
+
+    matchPrint.textContent = `Match number ${game.matchNumber}`;
+    playerElection.textContent = `Player chooses ${game.player}`;
+    computerElection.textContent = `Computer chooses ${game.computer}`;
+
+    messageContainer.appendChild(matchPrint);
+    messageContainer.appendChild(playerElection);
+    messageContainer.appendChild(computerElection);
+
+    return messageContainer;
+
+
+}
+
 const message = document.createElement("p");
 const game = {
     player: "",
@@ -92,9 +115,13 @@ const scissorsButton = document.querySelector("#play-buttons .scissors");
 rockButton.addEventListener("click", function(){
     game.player = "rock";
     playRound("rock", getComputerChoice());
+    
+    
   });
+
 paperButton.addEventListener("click", function(){
     playRound("paper", getComputerChoice());
+    
 });
 scissorsButton.addEventListener("click", function(){
     playRound("scissors", getComputerChoice());
